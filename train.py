1a0f14698d1e2f47231baf71c9bdebd5f45a3d9b
jim879546213-deep <74955844+jim879546213-deep@users.noreply.github.com>
2020-11-24 17:11:46 +0800

    Update train.py

--- train.py
+++ train.py
@@ -437,6 +437,7 @@ if __name__ == '__main__':
     parser.add_argument('--project', default='runs/train', help='save to project/name')
     parser.add_argument('--name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--save_dir',default='content/')
     opt = parser.parse_args()
 
     # Set DDP variables
